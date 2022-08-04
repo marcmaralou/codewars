ee73e291ff28dbe32c64c23fbb7634aa4d9f6357
marcmaralou <102846078+marcmaralou@users.noreply.github.com>
2022-08-04 12:30:24 -0700

    Sum of positive

---
+++ 8-kyu/sum-of-positive.js
@@ -0,0 +1,15 @@
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+
+function positiveSum(arr) {
+  sum = 0
+  arr.forEach((x) => {
+    if (x > 0) {
+      sum += x;
+    }
+  })
+  return sum;
+}
